eb9e21ac1c5d28449f47fb0a03dba40327048d1a
Gerald Nwankwo <geraldcjnwankwo@outlook.com>
2022-08-05 01:42:24 +0100

    changed db order

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const MongoStore = require('connect-mongo');
 
-const dbUrl = 'mongodb://localhost:27017/yelp-camp' || process.env.DB_URL;
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 const userRoutes = require('./routes/user');
 const campgroundRoutes = require('./routes/campaground');
